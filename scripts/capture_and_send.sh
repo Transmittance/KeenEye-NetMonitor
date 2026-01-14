0200d866bd4aa038fd69a3a399005a8349c73069
NXHhunter2 <usaloveman@mail.ru>
2026-01-14 23:18:38 +0500

    update capture script

--- scripts/capture_and_send.sh
+++ scripts/capture_and_send.sh
@@ -1,7 +1,10 @@
-#!/bin/sh                                                                                          
-                                                                                                   
-PCAP="/opt/captures/cap.pcap"                                                                      
-SERVER="http://192.168.1.130:5001/upload_pcap"                                                  
-                                                                                                   
-tcpdump -i br0 -w "$PCAP" -c 5000                                                                  
+#!/bin/sh
+
+PCAP_DIR="./captures"
+PCAP="$PCAP_DIR/cap.pcap"
+SERVER="http://192.168.1.130:5001/upload_pcap"
+
+mkdir -p "$PCAP_DIR"
+
+tcpdump -i br0 -w "$PCAP" -c 2500 -v
 curl -X POST -F "file=@$PCAP" "$SERVER"
